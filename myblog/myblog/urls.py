e123bd5cdd2bfc390f626bff1f1906541ec03038
Hossein Bahreini <hosseinbahreini.inbox@gmail.com>
2024-06-04 13:05:34 +0330

    blog
    panel : 127.0.0.1:8000/admin/
    user : admin
    pass : 12345678@

---
+++ myblog/myblog/urls.py
@@ -0,0 +1,9 @@
+# myblog/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
